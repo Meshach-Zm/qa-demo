f2cbd5b57b3931dd50f6deec0cd2c9ac8e4aa9e1
Misheck Zimba <zimbamisheck00@gmail.com>
2026-03-01 00:04:18 +0200

    Add configurations for cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+
+  projectId: 'ccavz3',
   e2e: {
     baseUrl: 'http://localhost:3000',
     specPattern: 'cypress/e2e/**/*.cy.js',
